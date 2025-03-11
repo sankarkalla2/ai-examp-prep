b228d27c8fc4a089b30c7bafe4da1802ea69a338
sankarkalla <gowrisankarkalla4@gmail.com>
2025-03-11 16:27:50 +0530

    this is my first commit i think

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,17 +1,19 @@
 "use client";
 
-import { UploadButton } from "@/lib/utils";
-
-
+import { getFile } from "@/actions/demo";
+import { UploadButton } from "@/lib/uploadthing";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <UploadButton
         endpoint="imageUploader"
-        onClientUploadComplete={(res) => {
+        onClientUploadComplete={async (res) => {
           // Do something with the response
           console.log("Files: ", res);
+          console.log("File: ", res[0].key);
+          const file = await getFile(res[0].key);
+
           alert("Upload Completed");
         }}
         onUploadError={(error: Error) => {
@@ -22,3 +24,6 @@ export default function Home() {
     </main>
   );
 }
+
+
+
